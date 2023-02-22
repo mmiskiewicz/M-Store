85f6cdc7cde49bf73f381b1bd560252b4fbee5af
Mateusz Miśkiewicz <br.miskiewicz@gmail.com>
2023-02-22 22:18:19 +0100

    add wtforms

---
+++ forms.py
@@ -0,0 +1,21 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import DataRequired, URL
+from flask_ckeditor import CKEditorField
+
+
+class LoginForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    submit = SubmitField("Log in")
+
+
+class RegisterForm(FlaskForm):
+    name = StringField("Name", validators=[DataRequired()])
+    surname = StringField("Surname", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    address = StringField("Address", validators=[DataRequired()])
+    postal_code = StringField("Postal code", validators=[DataRequired()])
+    city = StringField("City", validators=[DataRequired()])
+    submit = SubmitField("Sign up")
