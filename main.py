23647fe3a0c81117a75fd0aab715082bdac3c7b5
Mateusz Miśkiewicz <br.miskiewicz@gmail.com>
2023-02-23 11:57:51 +0100

    update login and register routes, add user table

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, flash
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_ckeditor import CKEditor
@@ -6,9 +6,11 @@ from sqlalchemy.orm import relationship
 from forms import LoginForm, RegisterForm
 from bs4 import BeautifulSoup
 from flask_wtf import FlaskForm
+from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
 from wtforms import StringField, SubmitField, BooleanField, SelectField
 from wtforms.validators import DataRequired, URL
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
@@ -18,6 +20,27 @@ Bootstrap(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shop.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class User(UserMixin, db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    surname = db.Column(db.String(100))
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    address = db.Column(db.String(100))
+    postal_code = db.Column(db.String(10))
+    city = db.Column(db.String(100))
+    country = db.Column(db.String(100))
+    cart = relationship("Cart", back_populates="user")
 
 
 class Product(db.Model):
@@ -42,12 +65,14 @@ class Cart(db.Model):
     __tablename__ = "shopping_cart"
     id = db.Column(db.Integer, primary_key=True)
     product_id = db.Column(db.Integer, db.ForeignKey("products.id"))
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
     size = db.Column(db.String(6), nullable=False)
     quantity = db.Column(db.Integer, nullable=False)
     product = relationship("Product", back_populates="cart")
+    user = relationship("User", back_populates="cart")
 
 # db.drop_all()
-# db.create_all()
+db.create_all()
 
 
 
@@ -84,6 +109,17 @@ def login():
     if form.validate_on_submit():
         email = form.email.data
         password = form.password.data
+        user = User.query.filter_by(email=email).first()
+        # Email doesn't exist or password incorrect.
+        if not user:
+            flash("That email does not exist, please try again.")
+            return redirect(url_for('login'))
+        elif not check_password_hash(user.password, password):
+            flash('Password incorrect, please try again.')
+            return redirect(url_for('login'))
+        else:
+            login_user(user)
+            return redirect(url_for('show_main_page'))
     return render_template("login.html", form=form)
 
 
@@ -91,13 +127,32 @@ def login():
 def register():
     form = RegisterForm()
     if form.validate_on_submit():
-        name = form.name.data
-        surname = form.surname.data
-        email = form.email.data
-        password = form.password.data
-        address = form.address.data
-        postal_code = form.postal_code.data
-        city = form.city.data
+        if User.query.filter_by(email=form.email.data).first():
+            print(User.query.filter_by(email=form.email.data).first())
+            # User already exists
+            flash("You've already signed up with that email, log in instead!")
+            return redirect(url_for('login'))
+
+        hash_and_salted_password = generate_password_hash(
+            form.password.data,
+            method='pbkdf2:sha256',
+            salt_length=8
+        )
+        new_user = User(
+        name=form.name.data,
+        surname=form.surname.data,
+        email=form.email.data,
+        password=hash_and_salted_password,
+        address=form.address.data,
+        postal_code=form.postal_code.data,
+        city=form.city.data,
+        country=form.country.data
+        )
+        db.session.add(new_user)
+        db.session.commit()
+        login_user(new_user)
+        return redirect(url_for("show_main_page"))
+
 
     return render_template("register.html", form=form)
 
